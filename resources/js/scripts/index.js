dc359009e2cd6608f8c9dde328b3983e14d04ca2
Shovit Thapa <shovit.thapa@sfgagency.com>
2019-01-05 19:13:18 +0200

    The movie db api set up

--- resources/js/scripts/index.js
+++ resources/js/scripts/index.js
@@ -2,12 +2,53 @@
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Index = function Index() {
-  _classCallCheck(this, Index);
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-  console.log('ES6 run');
-  this.name = "shovit";
-  console.log(this.name);
-};
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Index =
+/*#__PURE__*/
+function () {
+  function Index() {
+    _classCallCheck(this, Index);
+
+    this.api = "https://api.themoviedb.org/3/movie/404368/images";
+    this.api_key = "220db9d772820f55fd7e39476eef1d78";
+    this.url = this.api + "?api_key=" + this.api_key;
+    this.service();
+  }
+
+  _createClass(Index, [{
+    key: "service",
+    value: function service() {
+      var _this = this;
+
+      fetch(this.url, {
+        method: "GET"
+      }).then(function (response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw Error(response.statusText);
+        }
+      }).then(function (data) {
+        _this.fetchSuccess(data);
+      }).catch(function (data) {
+        console.log("some error occoured: " + data);
+      });
+    }
+  }, {
+    key: "fetchSuccess",
+    value: function fetchSuccess(data) {
+      if (Object.keys(data).length !== 0) {
+        console.log(data);
+      } else {
+        console.log('nothing has been returned');
+      }
+    }
+  }]);
+
+  return Index;
+}();
 
 new Index();
--- resources/js/src/index.js
+++ resources/js/src/index.js
@@ -1,11 +1,43 @@
 'use strict'
 
 class Index{
+    
     constructor(){
-        console.log('ES6 run')
-        console.log(this.name)
 
+        this.api="https://api.themoviedb.org/3/movie/404368/images"
+        this.api_key="220db9d772820f55fd7e39476eef1d78"
+        this.url=this.api+"?api_key="+this.api_key
+        this.image_api="https://image.tmdb.org/t/p/w500/"
+       
+        this.service()
+    }
 
+    service(){
+        fetch(this.url,{
+            method:"GET"
+        })
+        .then(response=>{
+            if(response.ok){
+                return response.json()
+            }else{
+                throw Error(response.statusText)
+            }
+        })
+        .then(data=>{
+          this.fetchSuccess(data)
+           
+        })
+        .catch(data=>{
+            console.log("some error occoured: "+ data)
+        })
+    }
+
+    fetchSuccess(data){
+        if(Object.keys(data).length!==0){
+            console.log(data)
+        }else{
+            console.log('nothing has been returned')
+        }
     }
 }
 
